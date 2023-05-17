eb13e59813e84d764bdd92cc039a498561c36922
zolantal <zoltan_antal@outlook.com>
2023-05-17 11:20:18 +0200

    Create getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+function getComputerChoice() {
+  let numberChoice = Math.floor(Math.random() * 3);
+    
+  if (numberChoice === 0) {
+    return "Rock";
+  } else if (numberChoice === 1) {
+    return "Paper";  
+  } else {
+    return "Scissors";
+  }
+}
